0623b42752ff267e0dc1dbeed03121dd281bd58c
rob <liquidgod@hotmail.com>
2021-08-30 18:04:27 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t rpetty2012/multi-client:latest -f rpetty2012/mutli-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rpetty2012/multi-server:latest -f rpetty2012/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rpetty2012/multi-worker:latest -f rpetty2012/mutli-worker:$SHA -f ./worker/Dokcerfile ./worker
+docker push rpetty2012/multi-client:latest
+docker push rpetty2012/multi-server:latest
+docker push rpetty2012/multi-worker:latest
+docker push rpetty2012/multi-client:$SHA
+docker push rpetty2012/multi-server:$SHA
+docker push rpetty2012/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=rpetty2012/multi-server:$SHA
+kubectl set image deployments/client-deployment client=rpetty2012/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rpetty2012/multi-worker:$SHA
