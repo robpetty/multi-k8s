7d18d56651afd614cfff1cb4dcd105eab48d3bcb
rob <liquidgod@hotmail.com>
2021-08-30 18:37:22 -0700

    added deployment scripts - typo in deploy script fixed

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t rpetty2012/multi-client:latest -f rpetty2012/mutli-client:$SHA -f ./client/Dockerfile ./client
-docker build -t rpetty2012/multi-server:latest -f rpetty2012/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t rpetty2012/multi-worker:latest -f rpetty2012/mutli-worker:$SHA -f ./worker/Dokcerfile ./worker
+docker build -t rpetty2012/multi-client:latest -t rpetty2012/mutli-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rpetty2012/multi-server:latest -t rpetty2012/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rpetty2012/multi-worker:latest -t rpetty2012/mutli-worker:$SHA -f ./worker/Dokcerfile ./worker
 docker push rpetty2012/multi-client:latest
 docker push rpetty2012/multi-server:latest
 docker push rpetty2012/multi-worker:latest
